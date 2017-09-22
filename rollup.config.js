d6d8973eb6edb556845edc93aa2c38be293a9d94
york yao <yaoao12306@outlook.com>
2017-09-22 15:38:38 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
-  entry: 'dist/index.js',
-  dest: 'dist/ease-in-out.min.js',
-  format: 'umd',
-  moduleName: 'EaseInOut',
-  plugins: [resolve(), uglify()]
+  input: 'dist/index.js',
+  name: 'EaseInOut',
+  plugins: [resolve(), uglify()],
+  output: {
+    file: 'dist/ease-in-out.min.js',
+    format: 'umd'
+  }
 }
