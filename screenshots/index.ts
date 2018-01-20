c06bd50a770488045ed9cac170b380eeea86fa3d
york yao <yaoao12306@outlook.com>
2018-01-20 10:47:14 +0800

    refactor: use tslint-config-standard as lint rule

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -1,17 +1,17 @@
-import * as puppeteer from "puppeteer";
+import * as puppeteer from 'puppeteer'
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" });
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
 
-    await page.goto(`http://localhost:8000/demo/`);
-    await page.waitFor(500);
-    await page.screenshot({ path: `screenshots/initial.png` });
+  await page.goto(`http://localhost:8000/demo/`)
+  await page.waitFor(500)
+  await page.screenshot({ path: `screenshots/initial.png` })
 
-    await page.click("button");
-    await page.waitFor(1000);
-    await page.screenshot({ path: `screenshots/scrolled.png` });
+  await page.click('button')
+  await page.waitFor(1000)
+  await page.screenshot({ path: `screenshots/scrolled.png` })
 
-    browser.close();
-})();
+  browser.close()
+})()
--- src/index.ts
+++ src/index.ts
@@ -2,129 +2,129 @@
  * @public
  */
 export abstract class Base {
-    protected count: number;
-    protected oldTimestamp: number;
-    protected targetValue: number;
-    protected duration: number;
-    private currentRequest: number;
-    constructor(protected updated: (currentValue: number) => void) { }
-    public start(initialValue: number, targetValue: number, duration = 500) {
-        this.targetValue = targetValue;
-        this.duration = duration;
-        this.count = 0;
-        this.oldTimestamp = performance.now();
-        this.requestAnimationFrame();
+  protected count: number
+  protected oldTimestamp: number
+  protected targetValue: number
+  protected duration: number
+  private currentRequest: number
+  constructor (protected updated: (currentValue: number) => void) { }
+  public start (initialValue: number, targetValue: number, duration = 500) {
+    this.targetValue = targetValue
+    this.duration = duration
+    this.count = 0
+    this.oldTimestamp = performance.now()
+    this.requestAnimationFrame()
+  }
+  public stop () {
+    if (this.currentRequest) {
+      window.cancelAnimationFrame(this.currentRequest)
     }
-    public stop() {
-        if (this.currentRequest) {
-            window.cancelAnimationFrame(this.currentRequest);
-        }
-    }
-    protected abstract step(newTimestamp: number): void;
-    protected requestAnimationFrame() {
-        this.stop();
-        this.currentRequest = window.requestAnimationFrame(timestamp => {
-            this.step(timestamp);
-        });
-    }
+  }
+  protected abstract step (newTimestamp: number): void
+  protected requestAnimationFrame () {
+    this.stop()
+    this.currentRequest = window.requestAnimationFrame(timestamp => {
+      this.step(timestamp)
+    })
+  }
 }
 
 /**
  * @public
  */
 export class EaseInOut extends Base {
-    private radius: number;
-    public start(initialValue: number, targetValue: number, duration = 500) {
-        super.start(initialValue, targetValue, duration);
-        this.radius = (targetValue - initialValue) / 2;
+  private radius: number
+  public start (initialValue: number, targetValue: number, duration = 500) {
+    super.start(initialValue, targetValue, duration)
+    this.radius = (targetValue - initialValue) / 2
+  }
+  protected step (newTimestamp: number) {
+    if (newTimestamp < this.oldTimestamp) {
+      this.requestAnimationFrame()
+      return
     }
-    protected step(newTimestamp: number) {
-        if (newTimestamp < this.oldTimestamp) {
-            this.requestAnimationFrame();
-            return;
-        }
-        this.count += Math.PI / this.duration * (newTimestamp - this.oldTimestamp);
-        if (this.count > Math.PI) {
-            this.updated(this.targetValue);
-            return;
-        }
-        this.updated(this.targetValue - Math.round(this.radius + this.radius * Math.cos(this.count)));
-        this.oldTimestamp = newTimestamp;
-        this.requestAnimationFrame();
+    this.count += Math.PI / this.duration * (newTimestamp - this.oldTimestamp)
+    if (this.count > Math.PI) {
+      this.updated(this.targetValue)
+      return
     }
+    this.updated(this.targetValue - Math.round(this.radius + this.radius * Math.cos(this.count)))
+    this.oldTimestamp = newTimestamp
+    this.requestAnimationFrame()
+  }
 }
 
 /**
  * @public
  */
 export class EaseIn extends Base {
-    private radius: number;
-    public start(initialValue: number, targetValue: number, duration = 500) {
-        super.start(initialValue, targetValue, duration);
-        this.radius = targetValue - initialValue;
+  private radius: number
+  public start (initialValue: number, targetValue: number, duration = 500) {
+    super.start(initialValue, targetValue, duration)
+    this.radius = targetValue - initialValue
+  }
+  protected step (newTimestamp: number) {
+    if (newTimestamp < this.oldTimestamp) {
+      this.requestAnimationFrame()
+      return
     }
-    protected step(newTimestamp: number) {
-        if (newTimestamp < this.oldTimestamp) {
-            this.requestAnimationFrame();
-            return;
-        }
-        this.count += Math.PI / 2 / this.duration * (newTimestamp - this.oldTimestamp);
-        if (this.count > Math.PI / 2) {
-            this.updated(this.targetValue);
-            return;
-        }
-        this.updated(this.targetValue - Math.round(this.radius * Math.cos(this.count)));
-        this.oldTimestamp = newTimestamp;
-        this.requestAnimationFrame();
+    this.count += Math.PI / 2 / this.duration * (newTimestamp - this.oldTimestamp)
+    if (this.count > Math.PI / 2) {
+      this.updated(this.targetValue)
+      return
     }
+    this.updated(this.targetValue - Math.round(this.radius * Math.cos(this.count)))
+    this.oldTimestamp = newTimestamp
+    this.requestAnimationFrame()
+  }
 }
 
 /**
  * @public
  */
 export class EaseOut extends Base {
-    private radius: number;
-    public start(initialValue: number, targetValue: number, duration = 500) {
-        super.start(initialValue, targetValue, duration);
-        this.radius = targetValue - initialValue;
+  private radius: number
+  public start (initialValue: number, targetValue: number, duration = 500) {
+    super.start(initialValue, targetValue, duration)
+    this.radius = targetValue - initialValue
+  }
+  protected step (newTimestamp: number) {
+    if (newTimestamp < this.oldTimestamp) {
+      this.requestAnimationFrame()
+      return
     }
-    protected step(newTimestamp: number) {
-        if (newTimestamp < this.oldTimestamp) {
-            this.requestAnimationFrame();
-            return;
-        }
-        this.count += Math.PI / 2 / this.duration * (newTimestamp - this.oldTimestamp);
-        if (this.count > Math.PI / 2) {
-            this.updated(this.targetValue);
-            return;
-        }
-        this.updated(this.targetValue - Math.round(this.radius - this.radius * Math.sin(this.count)));
-        this.oldTimestamp = newTimestamp;
-        this.requestAnimationFrame();
+    this.count += Math.PI / 2 / this.duration * (newTimestamp - this.oldTimestamp)
+    if (this.count > Math.PI / 2) {
+      this.updated(this.targetValue)
+      return
     }
+    this.updated(this.targetValue - Math.round(this.radius - this.radius * Math.sin(this.count)))
+    this.oldTimestamp = newTimestamp
+    this.requestAnimationFrame()
+  }
 }
 
 /**
  * @public
  */
 export class Linear extends Base {
-    private offset: number;
-    public start(initialValue: number, targetValue: number, duration = 500) {
-        super.start(initialValue, targetValue, duration);
-        this.offset = targetValue - initialValue;
+  private offset: number
+  public start (initialValue: number, targetValue: number, duration = 500) {
+    super.start(initialValue, targetValue, duration)
+    this.offset = targetValue - initialValue
+  }
+  protected step (newTimestamp: number) {
+    if (newTimestamp < this.oldTimestamp) {
+      this.requestAnimationFrame()
+      return
     }
-    protected step(newTimestamp: number) {
-        if (newTimestamp < this.oldTimestamp) {
-            this.requestAnimationFrame();
-            return;
-        }
-        this.count += (newTimestamp - this.oldTimestamp) / this.duration;
-        if (this.count > 1) {
-            this.updated(this.targetValue);
-            return;
-        }
-        this.updated(this.targetValue - Math.round(this.offset - this.offset * this.count));
-        this.oldTimestamp = newTimestamp;
-        this.requestAnimationFrame();
+    this.count += (newTimestamp - this.oldTimestamp) / this.duration
+    if (this.count > 1) {
+      this.updated(this.targetValue)
+      return
     }
+    this.updated(this.targetValue - Math.round(this.offset - this.offset * this.count))
+    this.oldTimestamp = newTimestamp
+    this.requestAnimationFrame()
+  }
 }
